a5e7e0d7cf53a8aa5e212ea68d3931ed5ef95cf8
MartinCherry <martins.kirsis@gmail.com>
2022-11-18 15:52:58 +0200

    Rolled back update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,9 @@ import { LoginComponent } from './pages/login/login.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PlannerComponent } from './pages/planner/planner.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
-import { UsersComponent } from './pages/users/users.component';
-import { UserCardComponent } from './pages/users/user-card/user-card.component';
 import { UserComponent } from './pages/user/user.component';
+import { UserCardComponent } from './pages/users/user-card/user-card.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
